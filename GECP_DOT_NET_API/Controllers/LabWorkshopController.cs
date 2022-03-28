e7b897418b71408ba235dafd274a81c42e8bb886
ADITYA PATEL <adityagpatel59@gmail.com>
2022-03-28 22:58:43 +0530

    Lab and Committee created

---
+++ GECP_DOT_NET_API/Controllers/LabWorkshopController.cs
@@ -0,0 +1,108 @@
+﻿using GECP_DOT_NET_API.Helper;
+using GECP_DOT_NET_API.Models;
+using GECP_DOT_NET_API.Repository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GECP_DOT_NET_API.Controllers
+{
+    [ApiController]
+    public class LabWorkshopController : ControllerBase
+    {
+        private ILabWorkshopRepo ilabworkshopRepo;
+        private IWebHostEnvironment _hostingEnvironment;
+
+        public LabWorkshopController(IWebHostEnvironment environment)
+        {
+            ilabworkshopRepo = new LabWorkshopRepo();
+            _hostingEnvironment = environment;
+        }
+
+        [HttpGet, Route("api/GetAllLabWorkshopDetails")]
+        public IActionResult GetLabWorkshopDetails()
+        {
+            var response = ilabworkshopRepo.GetAllLabWorkshopDetails();
+            return Ok(response);
+        }
+
+        [HttpPost, Route("api/AddLabWorkshopDetail")]
+        public IActionResult AddLabWorkshopDetail(IFormCollection collection)
+        {
+            var file = collection.Files.FirstOrDefault();
+            var labworkshopVM = new LabWorkshopVM();
+            TryUpdateModelAsync<LabWorkshopVM>(labworkshopVM);
+            string filepath = string.Empty;
+            string fileName = Guid.NewGuid().ToString() + "." + file.FileName.Split('.')[1];
+            string dir;
+            if (_hostingEnvironment.WebRootPath != null)
+            {
+                dir = Path.Combine(_hostingEnvironment.WebRootPath, "uploads/labworkshop/lab");
+            }
+            else
+            {
+                dir = "uploads/labworkshop/lab";
+
+            }
+            filepath = dir + "/" + fileName;
+            var fileUploadTask = FileUpload.SaveFile(file, filepath, dir);
+            fileUploadTask.Wait();
+            bool status = fileUploadTask.Result;
+            if (status)
+            {
+                labworkshopVM.Image = filepath;
+                return Ok(ilabworkshopRepo.AddLabWorkshopDetail(labworkshopVM));
+            }
+            return Ok();
+        }
+
+        [HttpPut, Route("api/UpdateLabWorkshopDetail")]
+        public IActionResult UpdateLabWorkshopDetail(IFormCollection collection)
+        {
+            var file = collection.Files.FirstOrDefault();
+            var labworkshopVM = new LabWorkshopVM();
+            TryUpdateModelAsync<LabWorkshopVM>(labworkshopVM);
+            string filepath = string.Empty;
+            string dir;
+            if (_hostingEnvironment.WebRootPath != null)
+            {
+                dir = Path.Combine(_hostingEnvironment.WebRootPath, "uploads/labworkshop/lab");
+            }
+            else
+            {
+                dir = "uploads/labworkshop/lab";
+
+            }
+            if (file != null && file.Length > 0)
+            {
+                string fileName = Guid.NewGuid().ToString() + "." + file.FileName.Split('.')[1];
+                filepath = dir + "/" + fileName;
+                var fileUploadTask = FileUpload.SaveFile(file, filepath, dir);
+                fileUploadTask.Wait();
+                bool status = fileUploadTask.Result;
+                if (status)
+                {
+                    labworkshopVM.Image = filepath;
+                    return Ok(ilabworkshopRepo.UpdateLabWorkshopDetail(labworkshopVM));
+                }
+            }
+            else
+            {
+                labworkshopVM.Image = string.Empty;
+                return Ok(ilabworkshopRepo.UpdateLabWorkshopDetail(labworkshopVM));
+            }
+            return Ok();
+        }
+
+        [HttpPut, Route("api/DeletePlacementDetail")]
+        public IActionResult DeleteLabWorkshopDetail(LabWorkshopVM labworkshopVM)
+        {
+            return Ok(ilabworkshopRepo.DeleteLabWorkshopDetail(labworkshopVM));
+        }
+    }
+}
--- GECP_DOT_NET_API/Helper/ExtensionMethods.cs
+++ GECP_DOT_NET_API/Helper/ExtensionMethods.cs
@@ -9,6 +9,78 @@ namespace GECP_DOT_NET_API.Helper
 {
     public static class ExtensionMethods
     {
+        #region --> CommitteeVM
+        public static CommitteeVM ToModel(this Committee obj)
+        {
+            return new CommitteeVM()
+            {
+                Id = obj.Id,
+                Name = obj.Name,
+                Image = obj.Image,
+                Description = obj.Description,
+                Slogan = obj.Slogan,
+                IsDeleted = obj.IsDeleted,
+                CreatedDate = obj.CreatedDate,
+                CreatedDateInt = obj.CreatedDateInt,
+                UpdatedDate = obj.UpdatedDate,
+                UpdatedDateInt = obj.UpdatedDateInt
+            };
+        }
+        public static Committee ToContext(this CommitteeVM obj)
+        {
+            return new Committee()
+            {
+                //Id = obj.Id,
+                Name = obj.Name,
+                Image = obj.Image,
+                Description = obj.Description,
+                Slogan = obj.Slogan,
+                IsDeleted = obj.IsDeleted,
+                CreatedDate = obj.CreatedDate,
+                CreatedDateInt = obj.CreatedDateInt,
+                UpdatedDate = obj.UpdatedDate,
+                UpdatedDateInt = obj.UpdatedDateInt
+            };
+        }
+
+        #endregion
+
+
+        #region --> LabWorkshopVM
+        public static LabWorkshopVM ToModel(this LabWorkshopDetail obj)
+        {
+            return new LabWorkshopVM()
+            {
+                Id = obj.Id,
+                DeptId = obj.DeptId,
+                Name = obj.Name,
+                Description = obj.Description,
+                Image = obj.Image,
+                IsDeleted = obj.IsDeleted,
+                CreatedDate = obj.CreatedDate,
+                CreatedDateInt = obj.CreatedDateInt,
+                UpdatedDate = obj.UpdatedDate,
+                UpdatedDateInt = obj.UpdatedDateInt
+            };
+        }
+        public static LabWorkshopDetail ToContext(this LabWorkshopVM obj)
+        {
+            return new LabWorkshopDetail()
+            {
+                //Id = obj.Id,
+                DeptId = obj.DeptId,
+                Name = obj.Name,
+                Description = obj.Description,
+                Image = obj.Image,
+                IsDeleted = obj.IsDeleted,
+                CreatedDate = obj.CreatedDate,
+                CreatedDateInt = obj.CreatedDateInt,
+                UpdatedDate = obj.UpdatedDate,
+                UpdatedDateInt = obj.UpdatedDateInt
+            };
+        }
+        #endregion
+
         #region --> FacultyDetailsVM
         public static FacultyDetailsVM ToModel(this FacultyDetail obj)
         {
