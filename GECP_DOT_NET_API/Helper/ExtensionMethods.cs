b66236291be21614ddde5e4cc2ba244a6aeb87ca
ADITYA PATEL <adityagpatel59@gmail.com>
2022-05-14 21:29:34 +0530

    working api WorkExperience

--- GECP_DOT_NET_API/Helper/ExtensionMethods.cs
+++ GECP_DOT_NET_API/Helper/ExtensionMethods.cs
@@ -19,7 +19,7 @@ namespace GECP_DOT_NET_API.Helper
                 DeptId = obj.DeptId,
                 DesignationId = obj.DesignationId,
                 IsDeleted = obj.IsDeleted,
-                CreatedDate = obj.CreatedDate,
+                CreatedDate = (DateTime)obj.CreatedDate,
                 CreatedDateInt = obj.CreatedDateInt,
                 UpdatedDate = obj.UpdatedDate,
                 UpdatedDateInt = obj.UpdatedDateInt
@@ -101,7 +101,7 @@ namespace GECP_DOT_NET_API.Helper
                 Designation = obj.Designation,
                 Expertise = obj.Expertise,
                 IsDeleted = obj.IsDeleted,
-                CreatedDate = obj.CreatedDate,
+                CreatedDate = (DateTime)obj.CreatedDate,
                 CreatedDateInt = obj.CreatedDateInt,
                 UpdatedDate = obj.UpdatedDate,
                 UpdatedDateInt = obj.UpdatedDateInt
