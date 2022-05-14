676e55da100e791602c54283742f104767d1a9fa
ADITYA PATEL <adityagpatel59@gmail.com>
2022-05-14 21:07:47 +0530

    working api publication

--- GECP_DOT_NET_API/Database/Tag.cs
+++ GECP_DOT_NET_API/Database/Tag.cs
@@ -10,7 +10,7 @@ namespace GECP_DOT_NET_API.Database
         public int Id { get; set; }
         public string Name { get; set; }
         public bool? IsDeleted { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
         public string CreatedDateInt { get; set; }
         public DateTime? UpdatedDate { get; set; }
         public string UpdatedDateInt { get; set; }
--- GECP_DOT_NET_API/Database/WorkExperience.cs
+++ GECP_DOT_NET_API/Database/WorkExperience.cs
@@ -18,7 +18,7 @@ namespace GECP_DOT_NET_API.Database
         public string Designation { get; set; }
         public string Expertise { get; set; }
         public bool? IsDeleted { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
         public string CreatedDateInt { get; set; }
         public DateTime? UpdatedDate { get; set; }
         public string UpdatedDateInt { get; set; }
