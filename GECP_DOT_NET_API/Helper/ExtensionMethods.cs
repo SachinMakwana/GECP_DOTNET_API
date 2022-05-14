071b241dd0be20b5b529974352a632f842eedaf0
ADITYA PATEL <adityagpatel59@gmail.com>
2022-05-14 22:34:45 +0530

    working api vision

--- GECP_DOT_NET_API/Helper/ExtensionMethods.cs
+++ GECP_DOT_NET_API/Helper/ExtensionMethods.cs
@@ -19,7 +19,7 @@ namespace GECP_DOT_NET_API.Helper
                 DeptId = obj.DeptId,
                 DesignationId = obj.DesignationId,
                 IsDeleted = obj.IsDeleted,
-                CreatedDate = obj.CreatedDate,
+                CreatedDate = (DateTime)obj.CreatedDate,
                 CreatedDateInt = obj.CreatedDateInt,
                 UpdatedDate = obj.UpdatedDate,
                 UpdatedDateInt = obj.UpdatedDateInt
@@ -94,7 +94,7 @@ namespace GECP_DOT_NET_API.Helper
                 DeptId = obj.DeptId,
                 Description = obj.Description,
                 IsDeleted = obj.IsDeleted,
-                CreatedDate = obj.CreatedDate,
+                CreatedDate = (DateTime)obj.CreatedDate,
                 CreatedDateInt = obj.CreatedDateInt,
                 UpdatedDate = obj.UpdatedDate,
                 UpdatedDateInt = obj.UpdatedDateInt
