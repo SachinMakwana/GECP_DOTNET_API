9a9a364fc937a0679967a888996d8c98fa881508
Sachin Makwana <schnmakwana11@gmail.com>
2022-08-21 13:32:50 +0530

    final change
    final

---
+++ GECP_DOT_NET_API/Controllers/GrievanceController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GECP_DOT_NET_API.Repository;
+using GECP_DOT_NET_API.Models;
+
+namespace GECP_DOT_NET_API.Controllers
+{
+    [ApiController]
+    public class GrievanceController : ControllerBase
+    {
+        IGrievanceRepo iGrievanceRepo;
+        public GrievanceController()
+        {
+            iGrievanceRepo = new GrievanceRepo();
+        }
+        //Get All the Grievance if 0
+        //else get Grievance by department id
+        [HttpGet,Route("api/GetAllGrievances")]
+        public IActionResult GetAllGrievance(int deptID=0)
+        {
+            return Ok(iGrievanceRepo.GetAllGrievances());
+        }
+        [HttpPost,Route("api/AddGrievance")]
+        public IActionResult AddGrievance(GrievanceVM grievanceVM)
+        {
+            return Ok(iGrievanceRepo.AddGrievance(grievanceVM));
+        }
+        [HttpPost, Route("api/UpdateGrievanceStatus")]
+        public IActionResult UpdateGrievanceStatus(GrievanceStatusVM grievanceStatusVM)
+        {
+            return Ok(iGrievanceRepo.AddGrievanceStatus(grievanceStatusVM));
+        }
+    }
+}
---
+++ GECP_DOT_NET_API/Repository/IGrievanceRepo.cs
@@ -0,0 +1,18 @@
+﻿using GECP_DOT_NET_API.Helper;
+using GECP_DOT_NET_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GECP_DOT_NET_API.Repository
+{
+    interface IGrievanceRepo
+    {
+        public ServiceResponse<IList<GrievanceWithStatusVM>> GetAllGrievances();
+
+        public ServiceResponse<bool> AddGrievance(GrievanceVM grievanceVM);
+
+        public ServiceResponse<bool> AddGrievanceStatus(GrievanceStatusVM grievanceStatusVM);
+    }
+}
