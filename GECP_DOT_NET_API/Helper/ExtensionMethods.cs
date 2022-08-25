9c03b34303376fd9e45bef482966a26150371aa6
ADITYA PATEL <adityagpatel59@gmail.com>
2022-08-26 02:22:58 +0530

    added

--- GECP_DOT_NET_API/Helper/ExtensionMethods.cs
+++ GECP_DOT_NET_API/Helper/ExtensionMethods.cs
@@ -6,7 +6,119 @@ namespace GECP_DOT_NET_API.Helper
 {
     public static class ExtensionMethods
     {
-      
+
+#region --> LabWorkshopVM
+        public static LabWorkshopVM ToModel(this LabWorkshopDetail obj)
+        {
+            return new LabWorkshopVM()
+            {
+                Id = obj.Id,
+                DeptId = obj.DeptId,
+                Name = obj.Name,
+                Description = obj.Description,
+                Image = obj.Image,
+                IsDeleted = obj.IsDeleted,
+                CreatedDate = obj.CreatedDate,
+                CreatedDateInt = obj.CreatedDateInt,
+                UpdatedDate = obj.UpdatedDate,
+                UpdatedDateInt = obj.UpdatedDateInt
+            };
+        }
+        public static LabWorkshopDetail ToContext(this LabWorkshopVM obj)
+        {
+            return new LabWorkshopDetail()
+            {
+                //Id = obj.Id,
+                DeptId = obj.DeptId,
+                Name = obj.Name,
+                Description = obj.Description,
+                Image = obj.Image,
+                IsDeleted = obj.IsDeleted,
+                CreatedDate = obj.CreatedDate,
+                CreatedDateInt = obj.CreatedDateInt,
+                UpdatedDate = obj.UpdatedDate,
+                UpdatedDateInt = obj.UpdatedDateInt
+            };
+        }
+        #endregion
+
+
+ #region --> CommitteeVM
+        public static CommitteeVM ToModel(this Committee obj)
+        {
+            return new CommitteeVM()
+            {
+                Id = obj.Id,
+                Name = obj.Name,
+                Image = obj.Image,
+                Description = obj.Description,
+                Slogan = obj.Slogan,
+                IsDeleted = obj.IsDeleted,
+                CreatedDate = obj.CreatedDate,
+                CreatedDateInt = obj.CreatedDateInt,
+                UpdatedDate = obj.UpdatedDate,
+                UpdatedDateInt = obj.UpdatedDateInt
+            };
+        }
+        public static Committee ToContext(this CommitteeVM obj)
+        {
+            return new Committee()
+            {
+                //Id = obj.Id,
+                Name = obj.Name,
+                Image = obj.Image,
+                Description = obj.Description,
+                Slogan = obj.Slogan,
+                IsDeleted = obj.IsDeleted,
+                CreatedDate = obj.CreatedDate,
+                CreatedDateInt = obj.CreatedDateInt,
+                UpdatedDate = obj.UpdatedDate,
+                UpdatedDateInt = obj.UpdatedDateInt
+            };
+        }
+
+        #endregion
+
+       #region --> AttachmentVM
+        public static AttachmentVM ToModel(this Attachment obj)
+        {
+            return new AttachmentVM()
+            {
+                Id = obj.Id,
+                Name = obj.Name,
+                Title = obj.Title,
+                Type = obj.Type,
+                Description = obj.Description,
+                Attachment1 = obj.Attachment1,
+                PageId = obj.PageId,
+                IsDeleted = obj.IsDeleted,
+                IsVisible = obj.IsVisible,
+                CreatedDate = obj.CreatedDate,
+                CreatedDateInt = obj.CreatedDateInt,
+                UpdatedDate = obj.UpdatedDate,
+                UpdatedDateInt = obj.UpdatedDateInt
+            };
+        }
+        public static Attachment ToContext(this AttachmentVM obj)
+        {
+            return new Attachment()
+            {
+                //Id = obj.Id,
+                Name = obj.Name,
+                Title = obj.Title,
+                Type = obj.Type,
+                Description = obj.Description,
+                Attachment1 = obj.Attachment1,
+                PageId = obj.PageId,
+                IsDeleted = obj.IsDeleted,
+                IsVisible = obj.IsVisible,
+                CreatedDate = obj.CreatedDate,
+                CreatedDateInt = obj.CreatedDateInt,
+                UpdatedDate = obj.UpdatedDate,
+                UpdatedDateInt = obj.UpdatedDateInt
+            };
+        }
+        #endregion
      
         #region --> FacultyDetailsVM
         public static FacultyDetailsVM ToModel(this FacultyDetail obj)
