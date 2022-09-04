9bea06aecda5c461c92c26a1602e9436d83bbeeb
ADITYA PATEL <adityagpatel59@gmail.com>
2022-09-04 23:25:57 +0530

    company api added

---
+++ GECP_DOT_NET_API/Repository/ICompanyRepo.cs
@@ -0,0 +1,15 @@
+﻿using GECP_DOT_NET_API.Helper;
+using GECP_DOT_NET_API.Models;
+using System;
+using System.Collections.Generic;
+
+namespace GECP_DOT_NET_API.Repository
+{
+    public interface ICompanyRepo
+    {
+        public ServiceResponse<List<CompanyVM>> GetAllCompanyDetails();
+        public ServiceResponse<bool> AddCompanyDetail(CompanyVM companyVM);
+        public ServiceResponse<bool> DeleteCompanyDetail(CompanyVM companyVM);
+        public ServiceResponse<bool> UpdateCompanyDetail(CompanyVM companyVM);
+    }
+}
