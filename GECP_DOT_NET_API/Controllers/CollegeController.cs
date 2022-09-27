4457740c3991090c55b4c474050f784681b52e3f
ADITYA PATEL <adityagpatel59@gmail.com>
2022-09-27 15:05:39 +0530

    college api created

---
+++ GECP_DOT_NET_API/Controllers/CollegeController.cs
@@ -0,0 +1,99 @@
+﻿using GECP_DOT_NET_API.Helper;
+using GECP_DOT_NET_API.Models;
+using GECP_DOT_NET_API.Repository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System;
+using System.Linq;
+
+namespace GECP_DOT_NET_API.Controllers
+{
+    public class CollegeController : ControllerBase
+    {
+        private ICollegeRepo icollegeRepo;
+        private IWebHostEnvironment _hostingEnvironment;
+
+        public CollegeController(IWebHostEnvironment environment)
+        {
+            icollegeRepo = new CollegeRepo();
+            _hostingEnvironment = environment;
+        }
+
+        [HttpGet, Route("api/GetCollegeDetail")]
+        public IActionResult GetCollegeDetail()
+        {
+            var response = icollegeRepo.GetCollegeDetail();
+            return Ok(response);
+        }
+
+        [HttpPost, Route("api/AddCollegeDetail")]
+        public IActionResult AddCollegeDetail(IFormCollection collection)
+        {
+            var file = collection.Files.FirstOrDefault();
+            var collegeVM = new CollegeVM();
+            TryUpdateModelAsync<CollegeVM>(collegeVM);
+            string filepath = string.Empty;
+            string fileName = Guid.NewGuid().ToString() + "." + file.FileName.Split('.')[1];
+            string dir;
+            if (_hostingEnvironment.WebRootPath != null)
+            {
+                dir = Path.Combine(_hostingEnvironment.WebRootPath, "uploads/college");
+            }
+            else
+            {
+                dir = "uploads/college";
+
+            }
+            filepath = dir + "/" + fileName;
+            var fileUploadTask = FileUpload.SaveFile(file, filepath, dir);
+            fileUploadTask.Wait();
+            bool status = fileUploadTask.Result;
+            if (status)
+            {
+                collegeVM.Image = filepath;
+                return Ok(icollegeRepo.AddCollegeDetail(collegeVM));
+            }
+            return Ok();
+        }
+
+        [HttpPut, Route("api/UpdateCollegeDetail")]
+        public IActionResult UpdateCollegeDetail(IFormCollection collection)
+        {
+            var file = collection.Files.FirstOrDefault();
+            var collegeVM = new CollegeVM();
+            TryUpdateModelAsync<CollegeVM>(collegeVM);
+            string filepath = string.Empty;
+            string dir;
+            if (_hostingEnvironment.WebRootPath != null)
+            {
+                dir = Path.Combine(_hostingEnvironment.WebRootPath, "uploads/college");
+            }
+            else
+            {
+                dir = "uploads/college";
+
+            }
+            if (file != null && file.Length > 0)
+            {
+                string fileName = Guid.NewGuid().ToString() + "." + file.FileName.Split('.')[1];
+                filepath = dir + "/" + fileName;
+                var fileUploadTask = FileUpload.SaveFile(file, filepath, dir);
+                fileUploadTask.Wait();
+                bool status = fileUploadTask.Result;
+                if (status)
+                {
+                    collegeVM.Image = filepath;
+                    return Ok(icollegeRepo.UpdateCollegeDetail(collegeVM));
+                }
+            }
+            else
+            {
+                collegeVM.Image = string.Empty;
+                return Ok(icollegeRepo.UpdateCollegeDetail(collegeVM));
+            }
+            return Ok();
+        }
+    }
+}
---
+++ GECP_DOT_NET_API/Repository/ICollegeRepo.cs
@@ -0,0 +1,15 @@
+﻿using GECP_DOT_NET_API.Helper;
+using GECP_DOT_NET_API.Models;
+using System.Collections.Generic;
+
+namespace GECP_DOT_NET_API.Repository
+{
+    public interface ICollegeRepo
+    {
+        public ServiceResponse<List<CollegeVM>> GetCollegeDetail();
+
+        public ServiceResponse<bool> AddCollegeDetail(CollegeVM collegeVM);
+
+        public ServiceResponse<bool> UpdateCollegeDetail(CollegeVM collegeVM);
+    }
+}
